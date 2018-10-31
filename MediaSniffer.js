224ad411946c3fa311b4a7ceeb3b8d626f6008d8
greatestever <greatestever@users.noreply.github.com>
2018-10-30 22:25:11 -0400

    Fixed some things.

--- MediaSniffer.js
+++ MediaSniffer.js
@@ -4,20 +4,25 @@ javascript:
 	var arr = [];
 	var strBody ="", content = ""; 
 	var images = document.images; 
+	var links = document.links;
 	var meta = document.getElementsByTagName('meta');
 	var videos = document.getElementsByTagName('video');
 	var audios = document.getElementsByTagName('audio');
 	var refresh = window.localStorage.getItem('refresh');
 	
-	if(!document.URL.includes("instagram.com/p/") && refresh !== null)
+	if (images.length <= 0 && links.length <= 0 && meta.length <= 0 && videos.length <= 0 && audios.length <= 0)
+	{
+		alert("No media was found.\nPlease refresh to try again.");
+	}
+	else if(!document.URL.includes("instagram.com/p/") && refresh !== null)
 	{
 		window.localStorage.removeItem("refresh");
 		getMedia();
 	}
 	else if (document.URL.includes("instagram.com/p/") && refresh === null)
 	{
-		location.reload();
 		window.localStorage.setItem('refresh', "1");
+		location.reload();
 	}
 	else
 	{
@@ -35,12 +40,25 @@ javascript:
 			}
 		} 
 		
+		for (var i = 0; i < links.length; i++)
+		{ 
+			content = links[i].href;
+			if (content.includes(".jpg") || content.includes(".png") || content.includes(".jpeg") || content.includes(".gif") || content.includes(".svg") || content.includes(".ico") || content.includes(".bmp") || content.includes(".mp4") || content.includes(".mp3") || content.includes(".ogg") || content.includes(".wav"))
+			{	
+				if (arr.indexOf(content) === -1)
+				{
+					arr.push(content); 
+					strBody += content; 
+				}
+			}
+		} 
+		
 		for (var i = 0; i < meta.length; i++)
 		{ 
 			content = meta[i].getAttribute('content');
 			if (content != null)
 			{
-				if (content.includes(".mp4") || content.includes(".mp3") || content.includes(".wav"))
+				if (content.includes(".jpg") || content.includes(".png") || content.includes(".jpeg") || content.includes(".gif") || content.includes(".svg") || content.includes(".ico") || content.includes(".bmp") || content.includes(".mp4") || content.includes(".mp3") || content.includes(".ogg") || content.includes(".wav"))
 				{
 					if (arr.indexOf(content) === -1)
 					{
@@ -107,168 +125,176 @@ javascript:
 			}
 		} 
 		
-		document.head.innerHTML = "";
-		var node = document.createElement('style');
-		node.innerHTML = 
-		`
-		/* The Modal (background) */
-		.modal {
-			display: block; /* Visible by default */
-			position: fixed; /* Stay in place */
-			z-index: 1; /* Sit on top */
-			left: 0;
-			top: 0;
-			width: 100%; /* Full width */
-			height: 100%; /* Full height */
-			overflow: auto; /* Enable scroll if needed */
-			background-color: rgb(0,0,0); /* Fallback color */
-			background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-		}
-
-		/* Modal Content/Box */
-		.modal-content {
-			background-color: #fefefe;
-			margin: 15% auto; /* 15% from the top and centered */
-			padding: 20px;
-			border: 1px solid #888;
-			width: 80%; /* Could be more or less, depending on screen size */
-		}
-
-		/* The Close Button */
-		.close {
-			color: #aaa;
-			float: right;
-			font-size: 25px;
-			font-weight: bold;
-			width: 50px;
-			height: 50px;
-			border: 2px solid #141414;
-			background-color: #fefefe;
-			border-radius: 50%;
-		}
-
-		.close:hover,
-		.close:focus {
-			color: black;
-			text-decoration: none;
-			cursor: pointer;
-		}
-		
-		/* Media */
-		#media {
-			margin-left: auto;
-			margin-right: auto;
-			margin: auto;
-			text-align: justify;
-			-ms-text-justify: distribute-all-lines;
-			text-justify: distribute-all-lines;
-		}
-		
-		#media img {
-			vertical-align: top;
-			display: inline-block;
-			*display: inline;
-			zoom: 1;
-			max-width:100px;
-			max-height:100px;
-			width:auto;
-			height:auto;
-			margin: 10px;
-			border: 2px solid #141414;
-		}
-		
-		#media video {
-			vertical-align: top;
-			display: inline-block;
-			*display: inline;
-			zoom: 1;
-			max-width:100px;
-			max-height:100px;
-			width:auto;
-			height:auto;
-			margin: 10px;
-			border: 2px solid #141414;
-		}
-		
-		#media audio {
-			margin: 10px;
-			border: 2px solid #141414;
-		}
-		
-		/* Stretch */
-		.stretch {
-			width: 100%;
-			display: inline-block;
-			font-size: 0;
-			line-height: 0
-		}
-		`;
-		document.body.appendChild(node);
-		
-		document.body.innerHTML += 
-		`
-		<div id="myModal" class="modal">
-
-		  <!-- Modal content -->
-		  <div class="modal-content">
-			<div>
-			<button class="close" id="close">&times;</button>
-			</div>
-			<div id="media"></div>
-		  </div>
-
-		</div>
-		`;
-		
-		function close() 
+		if (arr.length <= 0)
 		{
-			var modal = document.getElementById('myModal');
+			alert("No media was found.\nPlease refresh to try again.");
+		}
+		else
+		{
+			document.head.innerHTML = "";
+			var node = document.createElement('style');
+			node.innerHTML = 
+			`
+			/* The Modal (background) */
+			.modal {
+				display: block; /* Visible by default */
+				position: fixed; /* Stay in place */
+				z-index: 1; /* Sit on top */
+				left: 0;
+				top: 0;
+				width: 100%; /* Full width */
+				height: 100%; /* Full height */
+				overflow: auto; /* Enable scroll if needed */
+				background-color: rgb(0,0,0); /* Fallback color */
+				background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+			}
+
+			/* Modal Content/Box */
+			.modal-content {
+				background-color: #fefefe;
+				margin: 15% auto; /* 15% from the top and centered */
+				padding: 20px;
+				border: 1px solid #888;
+				width: 80%; /* Could be more or less, depending on screen size */
+				min-height: 100px;
+			}
+
+			/* The Close Button */
+			.close {
+				color: #aaa;
+				float: right;
+				font-size: 25px;
+				font-weight: bold;
+				width: 50px;
+				height: 50px;
+				border: 2px solid #141414;
+				background-color: #fefefe;
+				border-radius: 50%;
+			}
+
+			.close:hover,
+			.close:focus {
+				color: black;
+				text-decoration: none;
+				cursor: pointer;
+			}
 			
-			modal.style.display = "none";
-			location.reload();
-		}
-		
-		function expand()
-		{
-			window.location.href = this.src;
-		}
-		
-		var btn = document.getElementById('close');
-		btn.onclick = close;
-		
-		var div = document.getElementById('media');
-		
-		for (var i = 0; i < arr.length; i++)
-		{
-			if (arr[i].includes(".jpg") || arr[i].includes(".png") || arr[i].includes(".jpeg") || arr[i].includes(".gif") || arr[i].includes(".svg") || arr[i].includes(".ico") || arr[i].includes(".bmp"))
-			{
-				div.innerHTML += '<img src="' + arr[i] + '" />'; 
+			/* Media */
+			#media {
+				margin-left: auto;
+				margin-right: auto;
+				margin: auto;
+				text-align: justify;
+				-ms-text-justify: distribute-all-lines;
+				text-justify: distribute-all-lines;
 			}
-			else if (arr[i].includes(".mp4") || arr[i].includes(".ogg"))
-			{
-				div.innerHTML += '<video src="' + arr[i] + '" />'; 
+			
+			#media img {
+				vertical-align: top;
+				display: inline-block;
+				*display: inline;
+				zoom: 1;
+				max-width:100px;
+				max-height:100px;
+				width:auto;
+				height:auto;
+				margin: 10px;
+				border: 2px solid #141414;
 			}
-			else if (arr[i].includes(".mp3") || arr[i].includes(".ogg") || arr[i].includes(".wav"))
-			{
-				div.innerHTML += '<audio src="' + arr[i] + '" />'; 
+			
+			#media video {
+				vertical-align: top;
+				display: inline-block;
+				*display: inline;
+				zoom: 1;
+				max-width:100px;
+				max-height:100px;
+				width:auto;
+				height:auto;
+				margin: 10px;
+				border: 2px solid #141414;
 			}
-		}
-		var imagesBtn = document.getElementsByTagName('img');
-		for (var i = 0; i < imagesBtn.length; i++)
-		{
-			imagesBtn[i].onclick = expand;
-		}
-		var videosBtn = document.getElementsByTagName('video');
-		for (var i = 0; i < videosBtn.length; i++)
-		{
-			videosBtn[i].onclick = expand;
-			videosBtn[i].setAttribute("controls", "controls");
-		}
-		var audiosBtn = document.getElementsByTagName('audio');
-		for (var i = 0; i < audiosBtn.length; i++)
-		{
-			audiosBtn[i].onclick = expand;
-			audiosBtn[i].setAttribute("controls", "controls");
+			
+			#media audio {
+				margin: 10px;
+				border: 2px solid #141414;
+			}
+			
+			/* Stretch */
+			.stretch {
+				width: 100%;
+				display: inline-block;
+				font-size: 0;
+				line-height: 0
+			}
+			`;
+			document.body.appendChild(node);
+			
+			document.body.innerHTML += 
+			`
+			<div id="myModal" class="modal">
+
+			  <!-- Modal content -->
+			  <div class="modal-content">
+				<div>
+				<button class="close" id="close">&times;</button>
+				</div>
+				<div id="media"></div>
+			  </div>
+
+			</div>
+			`;
+			
+			function close() 
+			{
+				var modal = document.getElementById('myModal');
+				
+				modal.style.display = "none";
+				location.reload();
+			}
+			
+			function expand()
+			{
+				window.location.href = this.src;
+			}
+			
+			var btn = document.getElementById('close');
+			btn.onclick = close;
+			
+			var div = document.getElementById('media');
+			
+			for (var i = 0; i < arr.length; i++)
+			{
+				if (arr[i].includes(".jpg") || arr[i].includes(".png") || arr[i].includes(".jpeg") || arr[i].includes(".gif") || arr[i].includes(".svg") || arr[i].includes(".ico") || arr[i].includes(".bmp"))
+				{
+					div.innerHTML += '<img src="' + arr[i] + '" />'; 
+				}
+				else if (arr[i].includes(".mp4") || arr[i].includes(".ogg"))
+				{
+					div.innerHTML += '<video src="' + arr[i] + '" />'; 
+				}
+				else if (arr[i].includes(".mp3") || arr[i].includes(".ogg") || arr[i].includes(".wav"))
+				{
+					div.innerHTML += '<audio src="' + arr[i] + '" />'; 
+				}
+			}
+			var imagesBtn = document.getElementsByTagName('img');
+			for (var i = 0; i < imagesBtn.length; i++)
+			{
+				imagesBtn[i].onclick = expand;
+			}
+			var videosBtn = document.getElementsByTagName('video');
+			for (var i = 0; i < videosBtn.length; i++)
+			{
+				videosBtn[i].onclick = expand;
+				videosBtn[i].setAttribute("controls", "controls");
+			}
+			var audiosBtn = document.getElementsByTagName('audio');
+			for (var i = 0; i < audiosBtn.length; i++)
+			{
+				audiosBtn[i].onclick = expand;
+				audiosBtn[i].setAttribute("controls", "controls");
+			}
 		}
 	}
 
